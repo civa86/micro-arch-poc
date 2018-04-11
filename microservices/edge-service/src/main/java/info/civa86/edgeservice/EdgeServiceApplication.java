02b7f5e5f195ea57ca7d98ab2db85c599d8ab57e
Dario Civallero <dario.civallero@etiqa.it>
2018-04-11 14:51:41 +0200

    adding spring microservices

---
+++ microservices/edge-service/src/main/java/info/civa86/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,20 @@
+package info.civa86.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+}
--- microservices/uuid-service/src/main/java/info/civa86/uuidservice/UuidServiceApplication.java
+++ microservices/uuid-service/src/main/java/info/civa86/uuidservice/UuidServiceApplication.java
@@ -1,8 +1,10 @@
 package info.civa86.uuidservice;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UuidServiceApplication {
 	public static void main(String[] args) {

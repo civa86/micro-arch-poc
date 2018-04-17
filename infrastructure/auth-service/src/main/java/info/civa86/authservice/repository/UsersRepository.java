05652297069a881fc95ecc210ee757c2df056fb1
Dario Civallero <dario.civallero@gmail.com>
2018-04-17 20:47:18 +0200

    auth server

---
+++ infrastructure/auth-service/src/main/java/info/civa86/authservice/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package info.civa86.authservice.repository;
+
+import info.civa86.authservice.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByName(String username);
+}
---
+++ infrastructure/auth-service/src/main/java/info/civa86/authservice/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package info.civa86.authservice.service;
+
+import info.civa86.authservice.model.CustomUserDetails;
+import info.civa86.authservice.model.Users;
+import info.civa86.authservice.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<Users> usersOptional = usersRepository.findByName(username);
+
+        usersOptional.orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
+        return usersOptional.map(CustomUserDetails::new).get();
+    }
+}
